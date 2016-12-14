8828c4e0a4585f77e30d479ab26f090e187f8c18
chairuddinsyah <si294r@yahoo.com>
2016-12-14 15:25:56 +0700

    Update Commit

--- savereferrer.php
+++ savereferrer.php
@@ -40,7 +40,7 @@ $statement1->execute();
 // save referrer
 $sql2 = "UPDATE referral_almighty_ios "
         . "SET referrer = :referrer "
-        . "WHERE swrve_user_id = :user_id and referrer <> :referrer1";
+        . "WHERE swrve_user_id = :user_id and coalesce(referrer, '') <> :referrer1 ";
 $statement2 = $connection->prepare($sql2);
 $statement2->bindParam(":referrer", $data['referrer']);
 $statement2->bindParam(":user_id", $data['swrve_user_id']);
