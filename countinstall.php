6cde0e4478393511b97bcf44876ba2827f53ebc8
chairuddinsyah <si294r@yahoo.com>
2016-12-23 14:02:39 +0700

    Update Commit

--- countinstall.php
+++ countinstall.php
@@ -3,11 +3,21 @@
 defined('IS_DEVELOPMENT') OR exit('No direct script access allowed');
 
 $swrve_user_id = isset($params[1]) ? $params[1] : "";
+$count_install = isset($params[2]) ? $params[2] : "";
 
 if (trim($swrve_user_id) == "") {
     return array(
         "status" => FALSE,
         "message" => "Error: swrve_user_id is empty"
+    );
+}
+
+if (is_numeric($count_install)) {
+    return array(
+        'swrve_user_id' => $swrve_user_id,
+        'count_install' => $count_install,
+        'error' => 0,
+        'message' => 'Success'
     );
 }
 
